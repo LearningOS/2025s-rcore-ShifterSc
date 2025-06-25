03469f87cd7a52263c97f9cf808f150371c6f96f
ff06b5 <1487558909@qq.com>
2025-06-25 17:40:58 +0800

    ch3 1

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -3,7 +3,7 @@ use crate::{
     task::{exit_current_and_run_next, suspend_current_and_run_next},
     timer::get_time_us,
 };
-
+use crate::task::TASK_MANAGER;
 #[repr(C)]
 #[derive(Debug)]
 pub struct TimeVal {
@@ -41,5 +41,24 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
+
+    if _trace_request == 0 {
+        let p = _id as *const u8;
+        let x = unsafe {
+            *p
+        };
+        return x as isize;
+    }
+    else if _trace_request == 1 {
+        let p = _id as *mut u8;
+        unsafe {
+            *p = _data as u8;
+        };
+        return 0;
+    }
+    else if _trace_request == 2 {
+        return TASK_MANAGER.get_syscall_count(_id) as isize;
+    }
+
     -1
 }
